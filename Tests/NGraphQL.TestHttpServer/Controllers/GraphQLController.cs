e2eff41505a3b6c083fd4bce4b572d15c85ea43c
rivantsov@gmail.com <rivantsov@gmail.com>
2021-01-04 22:53:12 -0800

    wip, http server

---
+++ Tests/NGraphQL.TestHttpServer/Controllers/GraphQLController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NGraphQL.Server;
+
+namespace NGraphQL.TestHttpServer.Controllers {
+  [ApiController]
+  [Route("graphql")]
+  public class GraphQLController : ControllerBase {
+    GraphQLServer _server; 
+
+    public GraphQLController(GraphQLServer server) {
+      _server = server; 
+    }
+
+    [HttpPost]
+    public async Task<object> PostAsync([FromBody] GraphQLRequest request) {
+      var resp = await _server.ExecuteAsync(request);
+      object result;
+      if (resp.Errors != null && resp.Errors.Count > 0)
+        result = new { errors = resp.Errors, data = resp.Data};
+      else 
+        result = new { data = resp.Data};
+      return result; 
+    }
+    
+    [HttpGet]
+    public object Get() {
+      return "GET not implemented";
+    }
+
+    [HttpGet]
+    [Route("schema")]
+    public string GetSchema() {
+      return _server.Model.SchemaDoc;
+    }
+  }
+}
---
+++ Tests/NGraphQL.TestHttpServer/Startup.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NGraphQL.Server;
+using NGraphQL.TestApp;
+
+namespace NGraphQL.TestHttpServer
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers().AddNewtonsoftJson();
+      
+      // create server and Http graphQL server 
+      var thingsBizApp = new ThingsApp();
+      var thingsServer = new GraphQLServer(thingsBizApp);
+      var thingsModule = new ThingsGraphQLModule();
+      thingsServer.RegisterModules(thingsModule);
+      thingsServer.Initialize();
+      services.AddSingleton(thingsBizApp);
+      services.AddSingleton(thingsServer);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      //app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      // app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+
+      app.UseGraphiQLServer();
+    }
+
+  }
+}
