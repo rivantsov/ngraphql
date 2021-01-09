22641543f87d9b4906f0949f3969d974ad6db8fa
rivantsov@gmail.com <rivantsov@gmail.com>
2021-01-09 00:33:24 -0800

    minor edits

--- src/Tests/NGraphQL.Tests.HttpTests/_TestEnv.cs
+++ src/Tests/NGraphQL.Tests.HttpTests/_TestEnv.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-using Arrest;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Newtonsoft.Json;
@@ -21,38 +19,17 @@ namespace NGraphQL.Tests.HttpTests {
   public static class TestEnv {
     public static string ServiceUrl = "http://127.0.0.1:55000";
     public static string GraphQLEndPointUrl = ServiceUrl + "/graphql";
-    public static GraphQLHttpServer ThingsHttpServer;
-    public static RestClient RestClient;
-    public static GraphQLClient Client; 
-
-    public static string LogFilePath = "_graphQLHttpTests.log";
-    private static JsonSerializerSettings _serializerSettings;
-
-
+    public static GraphQLClient Client;
     static IWebHost _webHost;
+    public static string LogFilePath = "_graphQLHttpTests.log";
 
     public static void Initialize() {
-      if (ThingsHttpServer != null) //already initialized
+      if (Client != null) //already initialized
         return;
       if (File.Exists(LogFilePath))
         File.Delete(LogFilePath);
 
-      _serializerSettings = new JsonSerializerSettings() {
-        Formatting = Formatting.Indented,
-        ContractResolver = new DefaultContractResolver() { NamingStrategy = new CamelCaseNamingStrategy() }
-      };
-      _serializerSettings.Converters.Add(new StringEnumConverter()); // enums as strings, not ints
-
-      // create server and Http graphQL server 
-      var thingsBizApp = new ThingsApp();
-      var thingsServer = new GraphQLServer(thingsBizApp);
-      var thingsModule = new ThingsGraphQLModule();
-      thingsServer.RegisterModules(thingsModule); 
-      thingsServer.Initialize();
-      ThingsHttpServer = new GraphQLHttpServer(thingsServer);
-
       StartWebHost();
-      RestClient = new RestClient(GraphQLEndPointUrl);
       Client = new GraphQLClient(GraphQLEndPointUrl);
       Client.RequestCompleted += Client_RequestCompleted;
     }
@@ -84,7 +61,6 @@ namespace NGraphQL.Tests.HttpTests {
 Testing: {descr}
 ");
     }
-
 
     private static void Client_RequestCompleted(object sender, RequestCompletedEventArgs e) {
       LogCompletedRequest(e.Data);
--- src/Tests/NGraphQL.Tests.HttpTests/_TestStartup.cs
+++ src/Tests/NGraphQL.Tests.HttpTests/_TestStartup.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Internal;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using NGraphQL.Server;
+using NGraphQL.Server.AspNetCore;
+using NGraphQL.TestApp;
 
 namespace NGraphQL.Tests.HttpTests {
 
@@ -10,7 +13,6 @@ namespace NGraphQL.Tests.HttpTests {
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services) {
-      services.AddSingleton(TestEnv.ThingsHttpServer);
       // clear out default configuration - it installs Console output logger which sends out tons of garbage
       //  https://weblog.west-wind.com/posts/2018/Dec/31/Dont-let-ASPNET-Core-Default-Console-Logging-Slow-your-App-down
       services.AddLogging(config => {
@@ -23,12 +25,23 @@ namespace NGraphQL.Tests.HttpTests {
 
       app.UseRouting();
 
-      var server = TestEnv.ThingsHttpServer;
+      var server = CreateGraphQLHttpServer();
       app.UseEndpoints(endpoints => {
         endpoints.MapPost("graphql", async context => await server.HandleGraphQLHttpRequestAsync(context));
         endpoints.MapGet("graphql", async context => await server.HandleGraphQLHttpRequestAsync(context));
         endpoints.MapGet("graphql/schema", async context => await server.HandleGraphQLHttpRequestAsync(context));
       });
+    }
+
+    private GraphQLHttpServer CreateGraphQLHttpServer() {
+      // create server and Http graphQL server 
+      var thingsBizApp = new ThingsApp();
+      var thingsServer = new GraphQLServer(thingsBizApp);
+      var thingsModule = new ThingsGraphQLModule();
+      thingsServer.RegisterModules(thingsModule);
+      thingsServer.Initialize();
+      var server = new GraphQLHttpServer(thingsServer);
+      return server;
     }
 
   }
