0d966cf3c807747cb021f94e9a25cf8f15a120ad
rivantsov <rivantsov@gmail.com>
2024-04-12 01:29:57 -0700

    edited PushAll bat file

--- Nuget/PushAll.bat
+++ Nuget/PushAll.bat
@@ -1,6 +1,6 @@
 SET pver=1.7.0
 Echo Version: "%pver%"
-dir Nupkg\*.nupkg
+dir packages\*.nupkg
 @echo off
 setlocal
 :PROMPT
@@ -12,10 +12,10 @@ cd packages
 
 echo Publishing....
 :: When we push bin package, the symbols package is pushed automatically by the nuget util
-nuget push Nupkg\NGraphQL.%pver%.nupkg -source https://api.nuget.org/v3/index.json 
-nuget push Nupkg\NGraphQL.Client.%pver%.nupkg -source https://api.nuget.org/v3/index.json 
-nuget push Nupkg\NGraphQL.Server.%pver%.nupkg -source https://api.nuget.org/v3/index.json 
-nuget push Nupkg\NGraphQL.Server.AspNetCore.%pver%.nupkg -source https://api.nuget.org/v3/index.json 
+nuget push NGraphQL.%pver%.nupkg -source https://api.nuget.org/v3/index.json 
+nuget push NGraphQL.Client.%pver%.nupkg -source https://api.nuget.org/v3/index.json 
+nuget push NGraphQL.Server.%pver%.nupkg -source https://api.nuget.org/v3/index.json 
+nuget push NGraphQL.Server.AspNetCore.%pver%.nupkg -source https://api.nuget.org/v3/index.json 
 pause
 
 :END
